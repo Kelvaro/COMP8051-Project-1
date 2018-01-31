81cbbb106efa40e1fd817ee65d7fcb24b3ce1144
Kelvaro <blizzardkang@gmail.com>
2018-01-31 12:23:48 -0800

    Instantiate gunball prefab

--- Assets/Fire.cs
+++ Assets/Fire.cs
@@ -9,6 +9,7 @@ public class Fire : MonoBehaviour {
     public float GunVelocity;
     public float speed;
     private float gravity = 9.81f;
+    public GameObject gunball;
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +17,8 @@ public class Fire : MonoBehaviour {
         angle = (Mathf.Asin(gravity * GameObject.Find("Target").transform.position.z / Mathf.Pow(speed,2))/2 ) ;
         gunangle = angle * 180 / Mathf.PI;
         GameObject.Find("GunCentre").transform.eulerAngles = new Vector3(-gunangle, 0, 0);
-		
+
+        Instantiate(gunball, new Vector3(0,0,GameObject.Find("GunCe2ntre").transform.position.z), Quaternion.identity);
 	}
 	
 	// Update is called once per frame
