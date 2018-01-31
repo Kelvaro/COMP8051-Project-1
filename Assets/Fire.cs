07ccdb5aa3e43959ab4b34a17cb0a44aa08277c1
Kelvaro <blizzardkang@gmail.com>
2018-01-31 14:17:25 -0800

    editing some variables

--- Assets/Fire.cs
+++ Assets/Fire.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Fire : MonoBehaviour {
-    public float angle;
-    public float gunangle;
+    public float angle; //theta
+    public float gunangle; // angle of the gun
     public float GunVelocityInitial;
     public float GunVelocity;
     public float speed;
     private float gravity = 9.81f;
     public GameObject gunball;
     public float timeelapsed;
+    
+    public float frame;
     public float cAngle;
+    public float sAngle;
+   
 	// Use this for initialization
 	void Start () {
 
@@ -21,6 +25,7 @@ public class Fire : MonoBehaviour {
         angle = (Mathf.Asin(gravity * GameObject.Find("Target").transform.position.z / Mathf.Pow(speed,2))/2 ) ;
         gunangle = angle * 180 / Mathf.PI;
         cAngle = Mathf.Cos(gunangle * Mathf.PI/ 180);
+        sAngle = Mathf.Sin(gunangle * Mathf.PI / 180);
         GameObject.Find("GunCentre").transform.eulerAngles = new Vector3(-gunangle, 0, 0);
         
         Instantiate(gunball, new Vector3(0,0,GameObject.Find("GunCentre").transform.position.z), Quaternion.identity);
@@ -29,7 +34,9 @@ public class Fire : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
-        GunVelocity = speed;
-
+      //  GunVelocity = speed * ;
+        timeelapsed = Time.fixedDeltaTime * frame;
+        frame++;
+        
 	}
 }
