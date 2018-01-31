6cbfd0835a1ab98e036408a66d72679c0d7c493c
Kelvaro <blizzardkang@gmail.com>
2018-01-31 14:50:37 -0800

    Z and Y position works now

--- Assets/Fire.cs
+++ Assets/Fire.cs
@@ -8,35 +8,38 @@ public class Fire : MonoBehaviour {
     public float GunVelocityInitial;
     public float GunVelocity;
     public float speed;
-    private float gravity = 9.81f;
+    private float gravity = -9.81f;
     public GameObject gunball;
     public float timeelapsed;
-    
+    public float positionX;
+    public float positionY;
     public float frame;
     public float cAngle;
     public float sAngle;
    
 	// Use this for initialization
 	void Start () {
-
-       
-      
-
-        angle = (Mathf.Asin(gravity * GameObject.Find("Target").transform.position.z / Mathf.Pow(speed,2))/2 ) ;
+        angle = (Mathf.Asin(gravity * (GameObject.Find("Target").transform.position.z - GameObject.Find("GunCentre").transform.position.z) / Mathf.Pow(speed,2))/2 ) ;
         gunangle = angle * 180 / Mathf.PI;
         cAngle = Mathf.Cos(gunangle * Mathf.PI/ 180);
-        sAngle = Mathf.Sin(gunangle * Mathf.PI / 180);
-        GameObject.Find("GunCentre").transform.eulerAngles = new Vector3(-gunangle, 0, 0);
+        sAngle = -Mathf.Sin(gunangle * Mathf.PI / 180);
+        GameObject.Find("GunCentre").transform.eulerAngles = new Vector3(gunangle, 0, 0);
         
-        Instantiate(gunball, new Vector3(0,0,GameObject.Find("GunCentre").transform.position.z), Quaternion.identity);
+        Instantiate(gunball, GameObject.Find("GunCentre").transform.position, Quaternion.identity);
+        
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
-      //  GunVelocity = speed * ;
+        
         timeelapsed = Time.fixedDeltaTime * frame;
         frame++;
+        positionX = GameObject.Find("GunCentre").transform.position.z + speed * cAngle * timeelapsed;
+        positionY = ((speed * sAngle) * timeelapsed) + ((gravity / 2) * Mathf.Pow(timeelapsed,2));
+
+        GameObject.Find("Gunball(Clone)").transform.position = new Vector3(0,positionY,positionX);
+
         
 	}
 }
